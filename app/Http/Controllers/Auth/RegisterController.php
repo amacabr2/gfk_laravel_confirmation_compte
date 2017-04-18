a432deaa393698fa424123674c79561af6812d9a
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-18 18:53:23 +0200

    Redéfinition de la fonction register dans la classe RegisterController pour qu'il est un mail de confirmation lors de l'inscription

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -37,6 +39,13 @@ class RegisterController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        event(new Registered($user = $this->create($request->all())));
+        return redirect('/login')->with('succes', 'Votre compte à bien été crée, vous devez le confirmer avec l\'email que vous allez recevoir');
     }
 
     /**
