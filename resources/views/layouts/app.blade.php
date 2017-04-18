4a765a48bfa25bb5467cca0cfbfc3597dde72bc4
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-18 18:57:11 +0200

    Ajout dans app.blade.php => affichage 'success' ou 'error' s'il éxiste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,22 @@
             </div>
         </nav>
 
+        @if(session('success'))
+            <div class="container">
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            </div>
+        @endif
+
+        @if(session('error'))
+            <div class="container">
+                <div class="alert alert-danger">
+                    {{ session('success') }}
+                </div>
+            </div>
+        @endif
+
         @yield('content')
     </div>
 
