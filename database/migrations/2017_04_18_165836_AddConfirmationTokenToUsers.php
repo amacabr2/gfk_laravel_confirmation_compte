331c85c2154794e56cfc83b7463811c63e39862b
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-18 19:00:48 +0200

    Migration effectué

--- database/migrations/2017_04_18_165836_AddConfirmationTokenToUsers.php
+++ database/migrations/2017_04_18_165836_AddConfirmationTokenToUsers.php
@@ -14,7 +14,7 @@ class AddConfirmationTokenToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('confirmation_token')->nullable();
         });
     }
 
